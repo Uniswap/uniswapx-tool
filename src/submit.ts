0bf20f63f2861cdd8658e7586c884babc84b2e56
Mark Toda <toda.mark@gmail.com>
2024-04-01 16:07:13 -0400

    feat: add UniswapX V2

--- src/submit.ts
+++ src/submit.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import { CHAIN_ID, Config } from './config';
 
-export async function submitOrder(
+export async function submitV1Order(
   config: Config,
   encodedOrder: string,
   signature: string,
@@ -14,6 +14,40 @@ export async function submitOrder(
     signature,
     chainId: CHAIN_ID,
     quoteId: quoteId,
+  };
+  try {
+    const response = await axios.post(url, payload, {
+      headers: {
+        origin: 'https://app.uniswap.org',
+        accept: 'application/json, text/plain, */*',
+        'content-type': 'application/json',
+      },
+    });
+    if (response.status !== 201) {
+      throw new Error(`Order submission failed with ${response.status}`);
+    }
+    const { hash } = response.data;
+    console.log(`Order submitted with hash ${hash}`);
+    return hash;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+export async function submitV2Order(
+  config: Config,
+  encodedInnerOrder: string,
+  innerSig: string,
+  quoteId?: string
+) {
+  const url = `${config.uniswapAPIUrl}/v2/rfq`;
+  const payload = {
+    encodedInnerOrder,
+    innerSig,
+    tokenInChainId: CHAIN_ID,
+    tokenOutChainId: CHAIN_ID,
+    quoteId: quoteId,
+    requestId: quoteId,
   };
   try {
     const response = await axios.post(url, payload, {
